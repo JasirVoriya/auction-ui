10c0a7a43b500afdc41abfd0ecb0824352ea0595
JasirVoriya <jasirvoriya@gmail.com>
2023-12-03 23:01:52 +0800

    用container-box包裹，固定排版

--- src/pages/UserCenterPage.vue
+++ src/pages/UserCenterPage.vue
@@ -2,27 +2,29 @@
 const tagIndex = ref('0');
 </script>
 <template>
-  <div class="w-screen h-screen">
+  <div class="min-w-full min-h-screen">
     <div class="w-screen h-[100px] bg-rose-500 flex items-center">
-      <ContainerBox class="flex items-center justify-between">
+      <container-box class="flex items-center justify-between">
         <a href="/">
           <img src="@/assets/svg/hammer.svg" class="w-20 h-20" />
         </a>
         <span class="text-white text-2xl font-semibold">拍卖平台</span>
-      </ContainerBox>
+      </container-box>
     </div>
-
-    <div class="h-full flex items-center justify-center">
-      <div class=" w-full h-full">
-        <nut-tabs class="h-full" v-model="tagIndex" title-scroll direction="vertical">
+    <container-box>
+      <div class="flex items-center justify-center">
+        <nut-tabs v-model="tagIndex" title-scroll direction="vertical">
           <nut-tab-pane pane-key="0" title="个人中心">
             <profile />
           </nut-tab-pane>
           <nut-tab-pane pane-key="1" title="账号设置">
             <account-settings />
           </nut-tab-pane>
+          <nut-tab-pane pane-key="2" title="我的发布">
+            <my-post-goods />
+          </nut-tab-pane>
         </nut-tabs>
       </div>
-    </div>
+    </container-box>
   </div>
 </template>
